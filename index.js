d5c045c98f629a80c584cd65de1aa3180bb0d775
thecodeplanner <thecodeplanner@gmail.com>
2020-12-14 17:04:58 -0500

    commit tree 565748226148a6c490c87cf0cd1767691583483a

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+let random = Math.random() * 10
+
+
+let num3 = 22
+let num4 = 6
+let mod = num3 % num4
+
+let max = Math.max(4, 20, 18, 12)
